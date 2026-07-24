10a803efd869eef8f73402ccd11d753d52dd63de
Abhishek3917 <abhishekskumar709@gmail.com>
2026-07-25 02:03:19 +0530

    authn completed

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
-app.use(cookieParser)
+app.use(cookieParser())
 app.use("/api/auth", authRoutes)
 
 const PORT = process.env.PORT
